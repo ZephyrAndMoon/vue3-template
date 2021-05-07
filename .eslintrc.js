97e7db37d67c1b740398f0bb80f99d53dfe13ec8
zephyr <1024676057@qq.com>
2021-05-07 18:02:51 +0800

    fixed: 解决eslint无法识别路径别名问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,36 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
-    'plugin:prettier/recommended', // 添加 prettier 插件
+    'plugin:prettier/recommended' // 添加 prettier 插件
   ],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {},
-};
+  rules: {
+    'import/extensions': [2, 'never', { 'web.js': 'never', json: 'never' }],
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'import/no-unresolved': [2, { ignore: ['antd-mobile'] }]
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', path.resolve(__dirname, 'src')]],
+        extensions: ['.js', '.vue', '.ts']
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      },
+      'import/parses': {
+        '@typescript-eslint/parser': ['.ts', '.tsx']
+      }
+    }
+  }
+}
