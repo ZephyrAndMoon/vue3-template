8c9725c145c7db83040683b2304b923a5e23ff01
zephyr <1024676057@qq.com>
2021-05-08 11:06:24 +0800

    chore: 修改 Eslint 校验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   env: {
     browser: true,
@@ -16,9 +18,27 @@ module.exports = {
   },
   plugins: ['vue', '@typescript-eslint'],
   rules: {
-    'import/extensions': [2, 'never', { 'web.js': 'never', json: 'never' }],
-    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
-    'import/no-unresolved': [2, { ignore: ['antd-mobile'] }]
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/named': 'off',
+    'import/no-absolute-path': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/order': 'off',
+    'import/no-duplicates': 'off',
+    'import/no-self-import': 'off',
+    'import/no-cycle': 'off',
+    'import/no-useless-path-segments': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'no-console': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['state', 'config']
+      }
+    ]
   },
   settings: {
     'import/resolver': {
